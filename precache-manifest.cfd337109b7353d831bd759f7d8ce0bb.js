ef61657353c6452a7734e75b54b2eb0c3d1af8b8
Ankit Kapoor <59123713+Ankit-89@users.noreply.github.com>
2022-02-21 18:22:12 +0530

    Updates

---
+++ precache-manifest.cfd337109b7353d831bd759f7d8ce0bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff0765c717a665f546eb0bce2327640",
+    "url": "/cameraproject/index.html"
+  },
+  {
+    "revision": "702fb1796c82414ae0f8",
+    "url": "/cameraproject/static/css/main.fd7a125e.chunk.css"
+  },
+  {
+    "revision": "a96cfba02da6892bc77b",
+    "url": "/cameraproject/static/js/2.e7fa6d8a.chunk.js"
+  },
+  {
+    "revision": "7540180e027fee4aac08f5b74b8a356a",
+    "url": "/cameraproject/static/js/2.e7fa6d8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b29918577b50de95b7c9",
+    "url": "/cameraproject/static/js/3.650c0757.chunk.js"
+  },
+  {
+    "revision": "702fb1796c82414ae0f8",
+    "url": "/cameraproject/static/js/main.e07841f8.chunk.js"
+  },
+  {
+    "revision": "1e1460c3524c054e1d2c",
+    "url": "/cameraproject/static/js/runtime-main.9dca71dd.js"
+  }
+]);
